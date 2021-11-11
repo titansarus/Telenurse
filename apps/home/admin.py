4169c14df4c8ca012c1a4b6bb732503343f80b1c
thedrna <dornadehghani@ymail.com>
2021-11-12 02:04:45 +0330

    nurse & ad entities added to db, time zone fixed.

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -5,5 +5,8 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.contrib import admin
 
+from .models import Nurse, Ad
+
 # Register your models here.
-# admin.site.register()
+admin.site.register(Nurse)
+admin.site.register(Ad)
--- apps/home/models.py
+++ apps/home/models.py
@@ -4,7 +4,46 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.db import models
-from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Nurse(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    document = models.FileField(upload_to='documents/%Y/%m/%d')
+    location = models.CharField(max_length=1000, blank=True)
+    phone_number = models.CharField(max_length=11)
+
+    def __str__(self):
+        return f'Nurse with username {self.username} info: {self.first_name} {self.last_name}, {self.email}, ' \
+               f'{self.phone_number}'
+
+
+class Ad(models.Model):
+    SERVICE_TYPES = (
+        ('1', 'مراقبت از سالمند'),
+        ('2', 'مراقبت از افراد کم توان'),
+        ('3', 'خدمات سرپایی')
+    )
+    SEX = (
+        ('woman', 'خانم'),
+        ('man', 'آقا')
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=11)
+    address = models.CharField(max_length=1000)
+    start_time = models.DateField()
+    end_time = models.DateField()
+    service_type = models.CharField(max_length=20, choices=SERVICE_TYPES)
+    sex = models.CharField(max_length=10, choices=SEX)
+
+    def __str__(self):
+        return f'Ad info: {self.address}, {self.phone_number}, {self.service_type}, {self.start_time} until ' \
+               f'{self.end_time}'
+
 
