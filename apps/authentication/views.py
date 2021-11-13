08ff04a0852d06f349a8ec851e136e368b914caf
mahsa.ama <mahsa.ama1391@gmail.com>
2021-11-13 18:24:58 +0330

    Authentication files well formatted

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -3,7 +3,6 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-# Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, SignUpForm
@@ -23,12 +22,10 @@ def init_view(request):
 def login_view(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect("/")
+
     form = LoginForm(request.POST or None)
-
     msg = None
-
     if request.method == "POST":
-
         if form.is_valid():
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
@@ -50,7 +47,6 @@ def register_user(request):
 
     msg = None
     success = False
-
     if request.method == "POST":
         form = SignUpForm(request.POST, request.FILES)
         if form.is_valid():
@@ -58,12 +54,9 @@ def register_user(request):
             username = form.cleaned_data.get("username")
             raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
-
             msg = 'User created - please <a href="/login">login</a>.'
             success = True
-
             # return redirect("/login/")
-
         else:
             msg = "Form is not valid"
     else:
