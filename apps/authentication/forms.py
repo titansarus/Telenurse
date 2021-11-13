76057c59aefc70b9d52212798db6ef71dee960ab
mahsa.ama <mahsa.ama1391@gmail.com>
2021-11-13 14:03:16 +0330

    Nurse profile added

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -6,71 +6,74 @@ Copyright (c) 2019 - present AppSeed.us
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Nurse
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
-            attrs={
-                "placeholder": "Username",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Username", "class": "form-control"}
+        )
+    )
     password = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={
-                "placeholder": "Password",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Password", "class": "form-control"}
+        )
+    )
 
 
-class SignUpForm(UserCreationForm):
+class SignUpForm(forms.ModelForm):
     first_name = forms.CharField(
         widget=forms.TextInput(
-            attrs={
-                "placeholder": "First Name",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "First Name", "class": "form-control"}
+        )
+    )
 
     last_name = forms.CharField(
         widget=forms.TextInput(
-            attrs={
-                "placeholder": "Last Name",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Last Name", "class": "form-control"}
+        )
+    )
 
     username = forms.CharField(
         widget=forms.TextInput(
-            attrs={
-                "placeholder": "Username",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Username", "class": "form-control"}
+        )
+    )
     email = forms.EmailField(
-        widget=forms.EmailInput(
-            attrs={
-                "placeholder": "Email",
-                "class": "form-control"
-            }
-        ))
+        widget=forms.EmailInput(attrs={"placeholder": "Email", "class": "form-control"})
+    )
     password1 = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={
-                "placeholder": "Password",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Password", "class": "form-control"}
+        )
+    )
     password2 = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={
-                "placeholder": "Password check",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Password check", "class": "form-control"}
+        )
+    )
+    phone_number = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"placeholder": "Phone Number", "class": "form-control"}
+        )
+    )
+    document = forms.FileField(
+        widget=forms.ClearableFileInput(),
+        label="Document",
+        help_text="Maximum file size: 200MB",
+        max_length=200,
+    )
 
     class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+        model = Nurse
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+            "phone_number",
+            "document",
+        )
--- apps/home/views.py
+++ apps/home/views.py
@@ -12,9 +12,8 @@ from django.urls import reverse
 
 @login_required(login_url="/login/")
 def index(request):
-    context = {'segment': 'index'}
-
-    html_template = loader.get_template('home/index.html')
+    context = {"segment": "index"}
+    html_template = loader.get_template("home/index.html")
     return HttpResponse(html_template.render(context, request))
 
 
@@ -24,21 +23,17 @@ def pages(request):
     # All resource paths end in .html.
     # Pick out the html file name from the url. And load that template.
     try:
-
-        load_template = request.path.split('/')[-1]
-
-        if load_template == 'admin':
-            return HttpResponseRedirect(reverse('admin:index'))
-        context['segment'] = load_template
-
-        html_template = loader.get_template('home/' + load_template)
+        load_template = request.path.split("/")[-1]
+        if load_template == "admin":
+            return HttpResponseRedirect(reverse("admin:index"))
+        context["segment"] = load_template
+        html_template = loader.get_template("home/" + load_template)
         return HttpResponse(html_template.render(context, request))
 
     except template.TemplateDoesNotExist:
-
-        html_template = loader.get_template('home/page-404.html')
+        html_template = loader.get_template("home/page-404.html")
         return HttpResponse(html_template.render(context, request))
 
     except:
-        html_template = loader.get_template('home/page-500.html')
+        html_template = loader.get_template("home/page-500.html")
         return HttpResponse(html_template.render(context, request))
