2fb842cabc35e3a8d65965f1711d25d28b3392df
mahsa.ama <mahsa.ama1391@gmail.com>
2021-11-13 18:27:47 +0330

    Home files well formatted.

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -4,8 +4,7 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib import admin
-
 from .models import Ad
 
-# Register your models here.
+
 admin.site.register(Ad)
--- apps/home/models.py
+++ apps/home/models.py
@@ -6,17 +6,13 @@ Copyright (c) 2019 - present AppSeed.us
 from django.db import models
 
 
-# Create your models here.
 class Ad(models.Model):
     SERVICE_TYPES = (
-        ('1', 'مراقبت از سالمند'),
-        ('2', 'مراقبت از افراد کم توان'),
-        ('3', 'خدمات سرپایی')
+        ("1", "مراقبت از سالمند"),
+        ("2", "مراقبت از افراد کم توان"),
+        ("3", "خدمات سرپایی"),
     )
-    SEX = (
-        ('woman', 'خانم'),
-        ('man', 'آقا')
-    )
+    SEX = (("woman", "خانم"), ("man", "آقا"))
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=11)
@@ -26,9 +22,8 @@ class Ad(models.Model):
     service_type = models.CharField(max_length=20, choices=SERVICE_TYPES)
     sex = models.CharField(max_length=10, choices=SEX)
 
-
     def __str__(self):
-        return f'Ad info: {self.address}, {self.phone_number}, {self.service_type}, {self.start_time} until ' \
-               f'{self.end_time}'
-
-
+        return (
+            f"Ad info: {self.address}, {self.phone_number}, {self.service_type}, {self.start_time} until "
+            f"{self.end_time}"
+        )
