d8294c4cb7e85b1fe14e6b6d02921bf8fdc3e0d1
Dorna Dehghani <dornadehghani@gmail.com>
2021-11-14 23:32:49 +0330

    classes nurse and nurseAd added

--- apps/home/models.py
+++ apps/home/models.py
@@ -6,6 +6,21 @@ Copyright (c) 2019 - present AppSeed.us
 from django.db import models
 
 
+class Nurse(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    document = models.FileField(upload_to='documents/%Y/%m/%d')
+    location = models.CharField(max_length=1000, blank=True)
+    phone_number = models.CharField(max_length=11)
+
+    def __str__(self):
+        return f'Nurse with username {self.username} info: {self.first_name} {self.last_name}, {self.email}, ' \
+               f'{self.phone_number}'
+
+
 class Ad(models.Model):
     SERVICE_TYPES = (
         ("1", "مراقبت از سالمند"),
@@ -27,3 +42,9 @@ class Ad(models.Model):
             f"Ad info: {self.address}, {self.phone_number}, {self.service_type}, {self.start_time} until "
             f"{self.end_time}"
         )
+
+
+class NurseAd(models.Model):
+    nurse_id = models.CharField(max_length=10000)
+    ad_id = models.CharField(max_length=10000)
+    current = models.BooleanField()
