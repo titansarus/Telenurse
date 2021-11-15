d44bfc27f6a4e1161ecf40396f3e4937265219de
thedrna <dornadehghani@ymail.com>
2021-11-15 11:28:46 +0330

    nth

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -4,12 +4,13 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.urls import path
-from .views import login_view, register_user, init_view
+from .views import login_view, register_user, init_view, ads_view
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('init/', init_view, name='init'),
     path('login/', login_view, name="login"),
     path('register/', register_user, name="register"),
-    path("logout/", LogoutView.as_view(), name="logout")
+    path("logout/", LogoutView.as_view(), name="logout"),
+    # path('submit_ad/', ads_view, name='submit_ad'),
 ]
