5563237288d4289a5d410cbb6b1ea2f65b9d9ed0
mahsa.ama <mahsa.ama1391@gmail.com>
2021-11-15 15:38:32 +0330

    show list of nurses in admin panel.

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from apps.authentication.models import Nurse
+from django.shortcuts import render
 
 
 @login_required(login_url="/login/")
@@ -26,6 +28,9 @@ def pages(request):
         load_template = request.path.split("/")[-1]
         if load_template == "admin":
             return HttpResponseRedirect(reverse("admin:index"))
+        elif load_template == "nurse-list.html":
+            return list_of_nurses(request)
+        
         context["segment"] = load_template
         html_template = loader.get_template("home/" + load_template)
         return HttpResponse(html_template.render(context, request))
@@ -37,3 +42,8 @@ def pages(request):
     except:
         html_template = loader.get_template("home/page-500.html")
         return HttpResponse(html_template.render(context, request))
+
+@login_required(login_url="/login/")
+def list_of_nurses(request):
+    nurses = [nurse for nurse in Nurse.objects.all()]
+    return render(request, "home/nurse-list.html", {"nurses": nurses})
