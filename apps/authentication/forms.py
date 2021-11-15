6f8fe0010da5bc3d8af8a6115bfeff0a8c1bb2a1
bahar <baharkh127@gmail.com>
2021-11-15 11:02:07 +0330

    added custom user and deleted old tables

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -5,9 +5,9 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from .models import Nurse
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class LoginForm(forms.Form):
     username = forms.CharField(
@@ -20,6 +20,13 @@ class LoginForm(forms.Form):
             attrs={"placeholder": "Password", "class": "form-control"}
         )
     )
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "password",
+        )
 
 
 class SignUpForm(forms.ModelForm):
@@ -50,7 +57,7 @@ class SignUpForm(forms.ModelForm):
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={"placeholder": "Password check", "class": "form-control"}
+            attrs={"placeholder": "Confirm Password", "class": "form-control"}
         )
     )
     phone_number = forms.CharField(
@@ -66,7 +73,7 @@ class SignUpForm(forms.ModelForm):
     )
 
     class Meta:
-        model = Nurse
+        model = User
         fields = (
             "first_name",
             "last_name",
@@ -77,3 +84,17 @@ class SignUpForm(forms.ModelForm):
             "phone_number",
             "document",
         )
+
+    def clean_password(self):
+        password1 = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("password_confirm")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError( self.error_messages['password_mismatch'],
+                                         code='password_mismatch' )
+
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
