20a58bf52501c1e444ac1f66424fa43101dc2d0b
Dorna Dehghani <dornadehghani@gmail.com>
2021-11-15 09:55:59 +0330

    Update apps/home/models.py, apps/home/admin.py

--- apps/home/models.py
+++ apps/home/models.py
@@ -6,21 +6,6 @@ Copyright (c) 2019 - present AppSeed.us
 from django.db import models
 
 
-class Nurse(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    document = models.FileField(upload_to='documents/%Y/%m/%d')
-    location = models.CharField(max_length=1000, blank=True)
-    phone_number = models.CharField(max_length=11)
-
-    def __str__(self):
-        return f'Nurse with username {self.username} info: {self.first_name} {self.last_name}, {self.email}, ' \
-               f'{self.phone_number}'
-
-
 class Ad(models.Model):
     SERVICE_TYPES = (
         ("1", "مراقبت از سالمند"),
