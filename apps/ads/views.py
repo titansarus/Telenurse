2060a0478d7cb727213a6fd4a95f9a0e1c23960b
thedrna <dornadehghani@ymail.com>
2021-11-15 10:24:47 +0330

    ads files added

---
+++ apps/ads/views.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+# from .forms import LoginForm, SignUpForm
+from django.contrib.auth import get_user_model
+from django.http import HttpResponseRedirect
+
+
+def ads_view(request):
+    # return render(request, 'ads/submit-ads.html', {})
+    return render(request, 'ads/submit-ads.html', {})
