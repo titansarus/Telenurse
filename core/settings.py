297bbcee3fffef7519ab87deb1879ce059d56230
bahar <baharkh127@gmail.com>
2021-11-19 02:31:13 +0330

    changed appreance of submit-ad html page

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config("SECRET_KEY", default="S#perS3crEt_1122")
 # TODO
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config("DEBUG", default=True, cast=bool)
-DEBUG = False
+DEBUG = True
 
 # load production server from .env
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", config("SERVER", default="127.0.0.1") ,'.herokuapp.com']
