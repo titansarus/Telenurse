099cbbcdc21bb8506f839b6de2f5e82b1a335442
mahsa.ama <mahsa.ama1391@gmail.com>
2021-11-19 23:26:33 +0330

    Tests Done.

--- core/settings.py
+++ core/settings.py
@@ -139,7 +139,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-# USE_TZ = True
+USE_TZ = True
 
 #############################################################
 # SRC: https://devcenter.heroku.com/articles/django-assets
