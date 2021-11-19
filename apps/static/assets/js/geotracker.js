fc925b5ca4e128adf9f50ba23656f7064cea7aec
mahsa.ama <mahsa.ama1391@gmail.com>
2021-11-19 19:49:43 +0330

    Track the location with out refreshing page.

--- apps/static/assets/js/geotracker.js
+++ apps/static/assets/js/geotracker.js
@@ -1,20 +1,9 @@
 let myMap = null;
 let violetIcon = null;
-// let watchId;
-// localStorage.setItem("watchId", watchId);
+let watchId = null;
 let currentLocationMarker = null;
 let trackingMarkers = new Array(5);
 let trackingMarkerIndex = 0; // current counter
-
-function click_button_start(ad_id) {
-    href_location = ad_id.toString() + "/start/"
-    location.href = href_location
-}
-
-function click_button_end(ad_id) {
-    href_location = ad_id.toString() + "/end/"
-    location.href = href_location
-}
 
 function single_locate() {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -28,7 +17,7 @@ function single_locate() {
         }, { timeout: 5000, enableHighAccuracy: true });
 }
 
-function index_startup(username) {
+function index_startup() {
     if (window.location.protocol !== 'http:') {
         alert("Geolocation requires usage of HTTPS, and you're using HTTP. Trying to JS redirect...");
         window.location.protocol = "http:";
@@ -46,78 +35,34 @@ function index_startup(username) {
     }).addTo(myMap);
     myMap.setView([62.6050, 29.756], 13);
 
-    if (localStorage.getItem("watchId") == "1") {
-        continue_tracking(username, localStorage.getItem("ad_id"));
-    } else {
-        violetIcon = new L.Icon({
-            iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
-            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.4.0/images/marker-shadow.png',
-            iconSize: [25, 41],
-            iconAnchor: [12, 41],
-            popupAnchor: [1, -34],
-            shadowSize: [41, 41]
-        });
+    violetIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.4.0/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
 
-        navigator.geolocation.getCurrentPosition(function (position) {
-            // First location got
-            console.log("Moving to current location", position);
-            currentLocationMarker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(myMap);
-            myMap.panTo([position.coords.latitude, position.coords.longitude]);
-        },
-            function (positionError) {
-                alert("Geolocation failure");
-                console.debug(positionError.message);
-            }, { maximumAge: 5000, timeout: 5000, enableHighAccuracy: true });
-    }
-}
-
-function continue_tracking(username, ad_id) {
-    alert("continue tracking");
-    let tracking_name = username;
-    tracking_name.disabled = true;
-    var watchId = navigator.geolocation.watchPosition(function (position) {
-        console.log("Tracked new position", position);
-        if (trackingMarkers[trackingMarkerIndex]) {
-            // Remove oldest markeer
-            myMap.removeLayer(trackingMarkers[trackingMarkerIndex]);
-
-        }
-        trackingMarkers[trackingMarkerIndex] = L.marker([position.coords.latitude, position.coords.longitude], { icon: violetIcon });
-        trackingMarkers[trackingMarkerIndex].addTo(myMap);
-        trackingMarkerIndex++;
-        if (trackingMarkerIndex >= trackingMarkers.length) {
-            trackingMarkerIndex = 0;  // Rollover
-        }
-
-        let xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            // Handle error, in case of successful we don't care
-        };
-        xhttp.open("POST", tracking_point_url);
-        xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        let data = new URLSearchParams();
-        data.append('username', tracking_name);
-        data.append('timestamp', position.timestamp.toString());
-        data.append('ad_id', ad_id);
-        data.append('altitude', position.coords.altitude == null ? "" : position.coords.altitude.toString());
-        data.append('altitude_accuracy', position.coords.altitudeAccuracy == null ? "" : position.coords.altitudeAccuracy.toString());
-        data.append('accuracy', position.coords.accuracy.toString());
-        data.append('latitude', position.coords.latitude.toString());
-        data.append('longitude', position.coords.longitude.toString());
-        xhttp.send(data);
-
-    }, null, { timeout: 5000, enableHighAccuracy: true });
-    localStorage.setItem("watchId", watchId);
-    alert(localStorage.getItem("watchId") + " " + typeof localStorage.getItem("watchId") + " " + typeof watchId);
+    navigator.geolocation.getCurrentPosition(function (position) {
+        // First location got
+        console.log("Moving to current location", position);
+        currentLocationMarker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(myMap);
+        myMap.panTo([position.coords.latitude, position.coords.longitude]);
+    },
+        function (positionError) {
+            alert("Geolocation failure");
+            console.debug(positionError.message);
+        }, { maximumAge: 5000, timeout: 5000, enableHighAccuracy: true });
 }
 
 function start_tracking(username, ad_id) {
-    if (localStorage.getItem("watchId") == "1") {
-        alert("You're already tracking. ");
+    if (watchId) {
+        alert("You're already tracking. Stop it or refresh this page");
     } else {
         let tracking_name = username;
         tracking_name.disabled = true;
-        var watchId = navigator.geolocation.watchPosition(function (position) {
+        watchId = navigator.geolocation.watchPosition(function (position) {
             console.log("Tracked new position", position);
             if (trackingMarkers[trackingMarkerIndex]) {
                 // Remove oldest markeer
@@ -149,23 +94,18 @@ function start_tracking(username, ad_id) {
             xhttp.send(data);
 
         }, null, { timeout: 5000, enableHighAccuracy: true });
-        localStorage.setItem("watchId", watchId);
-        alert(localStorage.getItem("watchId") + " " + typeof localStorage.getItem("watchId") + " " + typeof watchId);
         localStorage.setItem("ad_id", ad_id);
     }
 }
 
 function stop_tracking(username, ad_id) {
-    alert(localStorage.getItem("watchId") + " " + typeof localStorage.getItem("watchId"));
-    if (localStorage.getItem("watchId") != "1") {
+    if (!watchId) {
         alert("You haven't started tracking this ad yet. Start tracking first. ");
     } else {
         if (localStorage.getItem("ad_id") != ad_id.toString()) {
-            alert("You have another Ad in progress. Stop it first. ");
+            alert("You have another Ad in progress or haven't started this task yet");
         } else {
-            navigator.geolocation.clearWatch(parseInt(localStorage.getItem("watchId")));
-            localStorage.setItem("watchId", "0");
-            alert("set to " + localStorage.getItem("watchId"));
+            navigator.geolocation.clearWatch(watchId);
             let tracking_name = username;
             tracking_name.disabled = false;
             let xhttp = new XMLHttpRequest();
