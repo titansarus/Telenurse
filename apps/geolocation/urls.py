5ed823a1e89287c15e7c432f7fa8b531e9e8ac55
mahsa.ama <mahsa.ama1391@gmail.com>
2021-11-19 22:58:29 +0330

    Geolocation tests and comments added.

--- apps/geolocation/urls.py
+++ apps/geolocation/urls.py
@@ -1,8 +1,4 @@
 from django.urls import path
 from apps.geolocation import views
 
-urlpatterns = [
-    path('tracking-points-list/', views.TrackingPointsListView.as_view(), name='tracking-points-list'),
-    path('routes/', views.RoutesListView.as_view(), name="routes-list"),
-    path('', views.IndexView.as_view(), name='tracking-index'),
-]
+urlpatterns = []
