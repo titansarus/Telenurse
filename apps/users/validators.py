c6e1fbf99305ac9a3d52bac8f68d32f659167067
mahsa.ama <mahsa.ama1391@gmail.com>
2021-11-20 11:44:02 +0330

    maximum uploaded file size is 20M now.

---
+++ apps/users/validators.py
@@ -0,0 +1,10 @@
+from django.core.exceptions import ValidationError
+
+
+def validate_file_size(value):
+    filesize = value.size
+
+    if filesize > 2097152:
+        raise ValidationError("The maximum file size that can be uploaded is 20MB")
+    else:
+        return value
