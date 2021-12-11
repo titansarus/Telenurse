60bc0a7f717875580d934b334d4bf44933623a04
AlirezaT99 <tajmirriahia@yahoo.com>
2021-12-12 01:00:50 +0330

    Update users/urls.py

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,13 +4,13 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.urls import path
-from .views import login_view, nurse_register_view, init_view, nurse_list_view
+from .views import login_view, register_view, init_view, nurse_list_view
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("init/", init_view, name="init"),
-    path("login/", login_view, name="login"),
-    path("register/", nurse_register_view, name="register"),
-    path("logout/", LogoutView.as_view(), name="logout"),
-    path("nurse-list/", nurse_list_view, name="nurse-list")
+    path('init/', init_view, name='init'),
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('nurse-list/', nurse_list_view, name='nurse-list')
 ]
