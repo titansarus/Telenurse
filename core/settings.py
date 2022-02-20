aa624797da3142f1b5092464e6a59877a7873fc1
Amirmahdi Namjoo <amirm137878@gmail.com>
2022-02-21 00:48:51 +0330

    Activate Email Template
    Signed-off-by: Amirmahdi Namjoo <amirm137878@gmail.com>

--- core/settings.py
+++ core/settings.py
@@ -191,5 +191,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'telenurse.group8@gmail.com'
+EMAIL_HOST_USER = env("EMAIL_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_PASS")
